391d15cd9619b6b3484af0b0465badbcaf7705f9
EC2 Default User <ec2-user@ip-172-31-1-154.eu-west-1.compute.internal>
2023-12-10 22:52:10 +0000

    added description to manage.py

--- mypictureblog/urls.py
+++ mypictureblog/urls.py
@@ -14,16 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from blog.views import SidebarView
 
 urlpatterns = [
-     path(
-        "", SidebarView.as_view(), name="landing_page"
-    ), 
+    path("", SidebarView.as_view(), name="landing_page"),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-    path('auth/',include("authapp.urls")),
-] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    path('auth/', include("authapp.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
