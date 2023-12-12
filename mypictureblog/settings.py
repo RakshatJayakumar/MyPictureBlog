2a926592f95d0be9cb3afc69700c74f51096f092
EC2 Default User <ec2-user@ip-172-31-1-154.eu-west-1.compute.internal>
2023-12-12 02:05:51 +0000

    f

--- mypictureblog/settings.py
+++ mypictureblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1kh=sj^n4rdo%ilnnaebzau5=ja82d3g+oly=i8leho2p7-kgg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['x22245766-mypictureblog-env.eba-avzakniw.ap-southeast-1.elasticbeanstalk.com']
 
